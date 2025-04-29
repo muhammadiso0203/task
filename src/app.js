552cf15ad090d1de45cfdc85626b11216d21421b
muhammadiso0203 <muhammadiso0203@gmail.com>
2025-04-29 16:42:12 +0500

    main

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
-import express, { Router } from 'express';
+import express from 'express';
 import { config } from 'dotenv';
-import { connectDB } from './db/index.js'
+import {connectDB} from './db/index.js'
+import morgan from 'morgan';
 import taskRouter from './routes/taskRoutes.js'
 import cookieParser from "cookie-parser"
 import userRouter from "./routes/userRoutes.js"
+import fs from "fs";
+import path from "path";
 config();
 
 const app = express()
@@ -12,6 +15,23 @@ const PORT = +process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 await connectDB();
+
+
+const __dirname = path.resolve();
+const filePath = fs.createWriteStream(path.join(__dirname, "acces.log"), {
+    flag: "a",
+});
+
+if (process.env.NODE_ENV === 'PRODUCTION') {
+    app.use(morgan('combined', {
+        stream: filePath
+    })
+    );
+} else {
+    app.use(morgan("dev")
+    );
+}
+
 
 app.use('/task', taskRouter);
 app.use("/user", userRouter);
